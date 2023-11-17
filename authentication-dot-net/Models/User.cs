e674ac83b14dbc3d310c20faa49c5db40140435b
Brady Stohler <58865862+bradyscode@users.noreply.github.com>
2023-11-17 11:54:38 -0500

    add method to check password complexity

--- authentication-dot-net/Models/User.cs
+++ authentication-dot-net/Models/User.cs
@@ -20,6 +20,10 @@ namespace authentication_dot_net.Models
 
         private Password HashPassword(string password)
         {
+            if (!IsPasswordComplexEnough(password))
+            {
+                throw new Exception("Password is not complex enough");
+            }
             //salt generation
             var salt = Guid.NewGuid().ToString();
             var saltedPassword = password + salt;
@@ -32,6 +36,36 @@ namespace authentication_dot_net.Models
             //store salt and hashedValue
             //when user enters password it will look in db at salt, add it to the salt and compute then compare. Makes sense
             return new Password(hashValue, salt);
+        }
+
+        private bool IsPasswordComplexEnough(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                return false;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                return false;
+            }
+
+            if (!password.Any(char.IsLower))
+            {
+                return false;
+            }
+
+            if (!password.Any(char.IsSymbol))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
